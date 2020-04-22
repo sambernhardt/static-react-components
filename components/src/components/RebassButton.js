6f73e8369c4efc3d8ec05236de67839e63f7bfaf
Samuel Bernhardt <sdbernhardt@gmail.com>
2020-04-21 20:37:45 -0400

    Removed bottom margin bottom

--- components/src/components/RebassButton.js
+++ components/src/components/RebassButton.js
@@ -4,7 +4,6 @@ import { Button } from 'rebass';
 export default ({ children, ...props }) => {
     return (
         <Button
-            mb="8px"
             dangerouslySetInnerHTML={{ __html: children }}
             {...props}
         />
